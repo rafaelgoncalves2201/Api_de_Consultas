86119801255f26a694acb80bf590adc136775247
Rafael Gonçalves <156006438+rafaelgoncalves2201@users.noreply.github.com>
2024-07-01 17:45:20 -0300

    Update App.py

--- Api EmpresaAqui/App.py
+++ Api EmpresaAqui/App.py
@@ -1,20 +1,30 @@
+# Importações necessarias 
 import requests
-import json
-import jmespath
 import pandas as pd
 
+# Variaveis
 nome_socios = []
 
+# Entrada de dados
 cnpj = input("Digite o cnpj que deseja pesquisar: ")
-token = "00cf4010b59bda8955c84e27296f715fdbfce7b0"
-link = f"https://www.empresaqui.com.br/api/{token}/{cnpj}"
 
+# Trata a entrada de dados para ficar assim 9999999999999 conforme solicita a API
+cn = cnpj.replace(".", "").replace("'", "").replace("-", "").replace("/", "").replace("_", "").replace("&", "")
+
+# Token para acesso a API de consulta
+token = "87df04458e30c64eeb8eac4a0348b6483d90e76c"
+
+# Junta a entrada de dados com o link e o token da api para a consulta
+link = f"https://www.empresaqui.com.br/api/{token}/{cn}"
+
+# Faz a pesquisa do link
 response = requests.get(link)
+
+# Converte o conteudo para Json
 data = response.json()
 
-# with open("Palin.json" ) as arq:
-#     data = json.load(arq)
 
+# Aqui a gente ta pegando as informações do Json
 razao_social = data.get("razao")
 cn = data.get("cnpj")
 telefone1 = f"{data.get('ddd_1')}{data.get('tel_1')}"
@@ -25,12 +35,16 @@ cnae2 = data.get("cnae_secundario")
 cidade = data.get("log_municipio")
 data_abertura = data.get("data_abertura")
 situacao = data.get("situacao_cadastral")
+
+# Aqui fiz um loop para pegar todos os socios que tiver
 for chave in data:
-    if chave.isdigit():  # Verifica se a chave é um número
+    if chave.isdigit():
         socio = data[chave]
         nome_socio = socio.get('socios_nome')
         if nome_socio:
             nome_socios.append(nome_socio)
+
+# Aqui estou usando o pandas para criar um dataframe e por as informações nume planilha
 df = pd.DataFrame({
     "Razão Social": [razao_social],
     "CNPJ": [cn],
@@ -42,5 +56,9 @@ df = pd.DataFrame({
     "Situação Empresa": [situacao],
     "Sócios": [", ".join(nome_socios)]
 })
+
+# Estou usando o pandas para salvar a planilha com o nome desejado
 df.to_excel('Informações.xlsx', index=False)
+
+# Aqui estou enviando uma mensagem para o usuario ficar sabendo que finalizou o processo
 print("Processo Finalizado com sucesso")
